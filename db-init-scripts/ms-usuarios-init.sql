2cec6023d1c7a8eaeb8185ce94e2dc3bec5f02f3
juan312 <juanalbani48@gmail.com>
2024-04-09 22:55:36 -0300

    Added sql script for ms-usuarios initialization

---
+++ db-init-scripts/ms-usuarios-init.sql
@@ -0,0 +1,6 @@
+INSERT INTO usr_tipo_usuario (id, tipo) VALUES
+(1, 'USUARIO_EMPRESA'),
+(2, 'VENDEDOR');
+
+INSERT INTO usr_clientes VALUES
+(1,'elpeiretti@gmail.com','20140113',true,100,'Empresa');
